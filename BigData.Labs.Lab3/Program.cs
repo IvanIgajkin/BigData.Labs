e8e7fd787ebdfebb823330c8e7c68d1e84beebe0
Ivan Igaikin <igaykiniv@gmail.com>
2022-09-18 18:43:02 +0300

    Finished?

--- BigData.Labs.Lab3/Program.cs
+++ BigData.Labs.Lab3/Program.cs
@@ -2,6 +2,7 @@
 
 using System.Globalization;
 using CsvHelper;
+using MathNet.Numerics.LinearAlgebra;
 
 using var streamReader = File.OpenText("C:/Users/igayk/Repos/BigData.Labs/BigData.Labs.Lab3/data.csv");
 using var csvReader = new CsvReader(streamReader, CultureInfo.CurrentCulture);
@@ -9,15 +10,78 @@ using var csvReader = new CsvReader(streamReader, CultureInfo.CurrentCulture);
 var data = csvReader.GetRecords<Data>().ToList();
 if (data.Any())
 {
+	//get factor regress model based on the input data
 	var regressModel = Enumerable.Range(1, 10)
-		.Select(facNo => new FactorRegressModel(data, facNo));
+		.Select(facNo => new FactorRegressModel(data, facNo))
+		.ToList();
+
+	var endogenousFactor = regressModel.FirstOrDefault(m => m.Factor == FactorRegressModel.FactorSymbol.Y);
+	if (endogenousFactor == null)
+		throw new ApplicationException($"Regress model doesn't contain resulting data");
 	
-	//TODO: реализовать алгоритм последовательного присоедениения регрессоров
+	double rDeterminationPast = default;
+	var regressors = new List<FactorRegressModel>();
+	int iterationNo = default;
+	while (iterationNo < regressModel.Count - 1)
+	{
+		FactorRegressModel? nextRegressor = null;
+		var maxCoefficient = double.MinValue;
+		if (endogenousFactor.Factor == FactorRegressModel.FactorSymbol.Y)
+		{
+			foreach (var regressor in regressModel)
+			{
+				if (regressor.Factor == FactorRegressModel.FactorSymbol.Y)
+				{
+					continue;
+				}
+
+				var correlation = endogenousFactor.Correlliation(regressor);
+				if (correlation > maxCoefficient)
+				{
+					maxCoefficient = correlation;
+					nextRegressor = regressor;
+				}
+			}
+		}
+		else
+		{
+			foreach (var regressor in regressModel)
+			{
+				if (regressor.Factor == FactorRegressModel.FactorSymbol.Y || regressors.Contains(regressor))
+				{
+					continue;
+				}
+
+				var tmpRegressorList = regressors.Append(regressor).ToList();
+				var correlationLists = tmpRegressorList.Select(r => tmpRegressorList.Select(r.Correlliation));
+				var determinant = Matrix<double>.Build.DenseOfColumns(correlationLists).Determinant();
+				if (determinant > maxCoefficient)
+				{
+					maxCoefficient = determinant;
+					nextRegressor = regressor;
+				}
+			}
+		}
+
+		if (nextRegressor == null || maxCoefficient < rDeterminationPast)
+		{
+			break;
+		}
+		
+		regressors.Add(nextRegressor);
+		endogenousFactor = regressors.Count == 1
+			? nextRegressor
+			: regressors.Aggregate((l, r) => l * r);
+		
+		rDeterminationPast = maxCoefficient;
+
+		iterationNo++;
+	};
+	
+	regressors.ForEach(r => Console.WriteLine($"Factor with index {r.Factor} is included"));
 }
 
 Console.ReadKey();
-
-//TODO: написать метод для вычисления корреляции
 
 static void ShowRegressModel(IEnumerable<FactorRegressModel> regressModel, IEnumerable<Data> data)
 {
@@ -105,16 +169,29 @@ class FactorRegressModel
 		_values2 = _values.Select(value => Math.Pow(value, 2.0));
 	}
 
+	public double Correlliation(FactorRegressModel right)
+	{
+		var multipleFactor = this * right;
+		return (multipleFactor.Average - Average * right.Average) / (AvgSqrt * right.AvgSqrt);
+	}
+
+	public static FactorRegressModel operator *(FactorRegressModel leftFactor, FactorRegressModel rightFactor)
+	{
+		var count = leftFactor._values.Count();
+		var values = new double[count];
+		for (int i = 0; i < count; i++)
+		{
+			values[i] = leftFactor._values.ElementAt(i) * rightFactor._values.ElementAt(i);
+		}
+
+		return new FactorRegressModel(values, FactorSymbol.Multiple);
+	}
+	
 	private FactorRegressModel(IEnumerable<double> values, FactorSymbol factor)
 	{
 		_values = values;
 		_values2 = _values.Select(value => Math.Pow(value, 2.0));
 		Factor = factor;
 	}
-
-	public static FactorRegressModel operator *(FactorRegressModel leftFactor, FactorRegressModel rightFactor) =>
-		new (leftFactor._values.Join(rightFactor._values,
-			l => l,
-			r => r,
-			(l, r) => l * r), FactorSymbol.Multiple);
+	
 }
